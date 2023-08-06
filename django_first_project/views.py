2be9d688f77953f492a26f0800c1a123d8f3f959
danugarri <danugarri@hotmail.com>
2023-08-06 17:30:58 +0200

    Added html templates

--- django_first_project/views.py
+++ django_first_project/views.py
@@ -1,30 +1,9 @@
-from django.http import HttpResponse
-from django.urls import reverse
-
-HTML_BODY = "<html><body></body></html>"
-GREETING = "<h1>Welcome to the Django Framework</h1>"
-
-DJANGO_REDIRECTION = '<a href="https://www.djangoproject.com/">Django</a>'
-LOGIN = '<a href="/admin/">Login</a>'
+from django.shortcuts import render
 
 
-def root(_):
-    REDIRECT_TO_GREETING = f'<a href="{reverse("greeting")}">greeting</a>'
-    return HttpResponse(
-        HTML_BODY
-        + "<ul>"
-        + "<li>"
-        + DJANGO_REDIRECTION
-        + "</li>"
-        + "<li>"
-        + REDIRECT_TO_GREETING
-        + "</li>"
-        + "<li>"
-        + LOGIN
-        + "</li>"
-    )
+def root(request):
+    return render(request, "root_template.html")
 
 
-def set_basic_view(_):
-    BACK = f'<a href="{reverse("root")}">Go Back</a>'
-    return HttpResponse(HTML_BODY + GREETING + "</br>" + BACK)
+def set_basic_view(request):
+    return render(request, "greeting_template.html")
