a5d727c4de0cfb4218716f84c26a802b9907945e
danugarri <danugarri@hotmail.com>
2023-08-08 20:29:58 +0200

    updated settings

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Add the following line to set the proper MIME type for CSS files.
+    "django.middleware.common.BrokenLinkEmailsMiddleware",
 ]
 
 ROOT_URLCONF = "firstProject.urls"
